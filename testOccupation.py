c5e0e870fb191f71f873ec766f2a10772577a69b
nfichter <nfichter@stuy.edu>
2016-09-15 21:36:27 -0400

    added tester function - running into KeyError: None

--- testOccupation.py
+++ testOccupation.py
@@ -25,5 +25,21 @@ def pickOccupation():
         passenger += occDict[occKey]
         if randNum < passenger:
             return occKey
-        
-print pickOccupation()
+
+buckets = {} #new dictionary that stores the number of times each occupation is picked
+          
+def test():
+  for line in occS: #creates each bucket, initializes each value to 0
+    comma = line.rfind(',')
+    occKey = line[:comma].strip('"')
+    buckets[occKey] = 0
+    
+  for num in range(10000): #runs 10000 times, picks one occupation and adds one to that bucket
+    chosenOne = pickOccupation()
+    print chosenOne
+    buckets[chosenOne] += 1
+
+  for key in occDict: #prints the key and its corresponding percentage (10000/100 = 100, so that will give percentage)
+    print key,buckets[key]/100
+
+test()
